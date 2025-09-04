bc88903db210d8f21e687a29b8de4037435d0175
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-09-04 19:46:04 -0300

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use App\External\ProductsClient;
+use GuzzleHttp\Client;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void {}
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,4 @@
+<?php
+
+use App\Http\Controllers\ClientsController;
+use Illuminate\Support\Facades\Route;
