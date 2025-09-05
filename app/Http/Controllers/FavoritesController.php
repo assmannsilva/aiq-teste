02320f04ce5ef248578ffd4cd76b6b185d6603c8
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-09-04 22:08:10 -0300

    feat: favorite controller

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFavoriteFormRequest;
+use App\Services\FavoriteService;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+
+    public function __construct(private FavoriteService $favoriteService) {}
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $client = $request->user();
+        $perPage = $request->input("per_page");
+
+        $products = $this->favoriteService->listClientFavorites($client, $perPage);
+        return \response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFavoriteFormRequest $request)
+    {
+        $client = $request->user();
+        $product = $this->favoriteService->addFavoriteIntoClient($client, $request->input("product_id"));
+        return \response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/StoreFavoriteFormRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreFavoriteFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $clientId = $this->user();
+        return [
+            "product_id" => [
+                "required",
+                "numeric",
+                Rule::unique("favorites", "product_id")->where(fn($query) => $query->where("client_id", $clientId))
+            ]
+        ];
+    }
+}
