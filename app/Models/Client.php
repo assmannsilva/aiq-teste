fa6775ab6d596aa61cef7cb25191e9498240f7d6
Cauê Assmann Silva <caueassmannsilva114@gmail.com>
2025-09-06 09:22:41 -0300

    fix: Client extending from User Authenticatable

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Client extends Model
+class Client extends Authenticatable
 {
     use HasFactory, HasUuids, HasApiTokens;
 
